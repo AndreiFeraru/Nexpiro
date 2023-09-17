13a29f4a3a1e1547e9e22fc2c02c06c940bae3dd
Andrei Feraru <feraruandrei97@gmail.com>
2023-09-17 09:29:26 +0300

    Initialize project

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDA19uWnzdyp2z8hwz2KZ9yZIW6uMHhCoY",
+    authDomain: "nexpiro-firebase.firebaseapp.com",
+    projectId: "nexpiro-firebase",
+    storageBucket: "nexpiro-firebase.appspot.com",
+    messagingSenderId: "200709942102",
+    appId: "1:200709942102:web:b1e24fed98503643ec84f7",
+    measurementId: "G-RPT43DZSWH"
+  },
+};
