84acd087e989853c50e9cfbed89495d1dd33a980
Andrei Feraru <feraruandrei97@gmail.com>
2024-02-18 14:04:48 +0200

    Refactor auth init

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
@@ -6,11 +7,15 @@ import { AuthService } from 'src/app/shared/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private auth: AuthService) {}
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.auth.isLoggedIn()) this.router.navigate(['/dashboard']);
+  }
 
   login() {
     if (this.email == '') {
