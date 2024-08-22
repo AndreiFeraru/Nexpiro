aac59d5a3409861f27d78746395f1f9bcc4bf99a
Andrei Feraru <feraruandrei97@gmail.com>
2024-08-22 18:52:58 +0300

    refactor: Add 'DotGothic16' font to extend fontFamily in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss';
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dot-matrix': ['"DotGothic16"', 'sans-serif'], // Add Google Font here
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
