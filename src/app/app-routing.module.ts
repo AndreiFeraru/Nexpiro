0ec1eb0fd38a8e85a20dc90fe9c48179b7a91a29
Andrei Feraru <feraruandrei97@gmail.com>
2024-09-12 20:48:27 +0300

    refactor: Update app-routing module to include new auth components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcceptInviteComponent } from './component/accept-invite/accept-invite.component';
+import { ForgotPasswordComponent } from './component/auth/forgot-password/forgot-password.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { RegisterComponent } from './component/auth/register/register.component';
+import { VerifyEmailComponent } from './component/auth/verify-email/verify-email.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
-import { LoginComponent } from './component/login/login.component';
 import { ManageStoragesComponent } from './component/manage-storages/manage-storages.component';
-import { RegisterComponent } from './component/register/register.component';
-import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
 import { ViewStorageComponent } from './component/view-storage/view-storage.component';
 
 const routes: Routes = [
