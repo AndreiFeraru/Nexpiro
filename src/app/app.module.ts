0c6beffac35fbdc90a301b0a64abae52963d7d6e
Andrei Feraru <feraruandrei97@gmail.com>
2024-09-16 21:18:40 +0300

    Refactor app to get firebase config from local, git-ignored .env file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {
   getDatabase,
   provideDatabase,
 } from '@angular/fire/database';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.dev';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToastComponent } from './component/toast/toast.component';
