10f4bfb8abc9d02612a435c6a506a6747b3c2c97
Andrei Feraru <feraruandrei97@gmail.com>
2025-07-21 12:00:03 +0300

    Refactor environment file generation

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,37 +1,49 @@
 require('dotenv').config();
-
 const fs = require('fs');
 const path = require('path');
 
-// TODO: Fail if any of these env variables are not set
-const env = {
-  production: true,
+const requiredEnvVars = [
+  'FIREBASE_API_KEY',
+  'FIREBASE_AUTH_DOMAIN',
+  'FIREBASE_DATABASE_URL',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID',
+  'FIREBASE_APP_ID',
+  'FIREBASE_MEASUREMENT_ID',
+  'OCR_API_KEY'
+];
+
+const baseEnv = {
   firebaseConfig: {
-    apiKey: process.env['FIREBASE_API_KEY'] || 'default_api_key',
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || 'default_auth_domain',
-    databaseURL: process.env['FIREBASE_DATABASE_URL'] || 'default_database_url',
-    projectId: process.env['FIREBASE_PROJECT_ID'] || 'default_project_id',
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || 'default_storage_bucket',
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || 'default_messaging_sender_id',
-    appId: process.env['FIREBASE_APP_ID'] || 'default_app_id',
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || 'default_measurement_id',
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+    databaseURL: process.env['FIREBASE_DATABASE_URL'],
+    projectId: process.env['FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_ID'],
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
   },
-  ocrApiKey: process.env['OCR_API_KEY'] || 'helloworld', // TODO add actual OCR API Key in GitHub Secrets
+  ocrApiKey: process.env['OCR_API_KEY'], // TODO add actual OCR API Key in GitHub Secrets
 };
 
-const content = `export const environment = ${JSON.stringify(env, null, 2)};\n`;
+const checkMissingEnvVars = (vars) => {
+  const missingVars = vars.filter(envVar => !process.env[envVar]);
+  if (missingVars.length > 0) {
+    console.error(`Error: Missing the following environment variables: ${missingVars.join(', ')}`);
+    process.exit(1);
+  }
+};
 
-const filePath = path.join(__dirname, '../src/environments/environment.prod.ts');
-fs.mkdirSync(path.dirname(filePath), { recursive: true });
-fs.writeFileSync(filePath, content, { flag: 'w' });
+const generateEnvFile = (env, filePath) => {
+  const content = `export const environment = ${JSON.stringify(env, null, 2)};\n`;
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  fs.writeFileSync(filePath, content, { flag: 'w' });
+  console.log(`${path.basename(filePath)} environment file generated.`);
+};
 
-console.log('Production environment file generated.');
+checkMissingEnvVars(requiredEnvVars);
 
-env.production = false;
-const contentDev = `export const environment = ${JSON.stringify(env, null, 2)};\n`;
-
-const filePathDev = path.join(__dirname, '../src/environments/environment.dev.ts');
-fs.mkdirSync(path.dirname(filePathDev), { recursive: true });
-fs.writeFileSync(filePathDev, contentDev, { flag: 'w' });
-
-console.log('Development environment file generated.');
+generateEnvFile({ ...baseEnv, production: true }, path.join(__dirname, '../src/environments/environment.prod.ts'));
+generateEnvFile({ ...baseEnv, production: false }, path.join(__dirname, '../src/environments/environment.dev.ts'));
